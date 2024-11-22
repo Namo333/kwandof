33d4fd42e5f6a5d3cb74beaa0e6afaf87595e5b4
Namo333 <dad688634@gmail.com>
2024-11-22 15:57:40 +0300

    b

--- backend/api/main.py
+++ backend/api/main.py
@@ -5,14 +5,14 @@ from api.routers import form_router
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:3000",
-    "https://kwandof-1iev.vercel.app/"
-]
+# origins = [
+#     "http://localhost:3000",
+#     "https://kwandof-1iev.vercel.app/"
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"], 
